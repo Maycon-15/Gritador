f003db4c48d89d499a2c2b577af74dd255c07ae7
Maycon-15 <78987309+Maycon-15@users.noreply.github.com>
2021-03-14 22:14:26 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FraseDoUsuario;
+            Console.WriteLine("Olá usuario, por favor, digite uma frase qualquer");
+            FraseDoUsuario = Console.ReadLine();
+
+            Console.WriteLine($"Você digitou {FraseDoUsuario.ToUpper()}");
+        }
+    }
+}
